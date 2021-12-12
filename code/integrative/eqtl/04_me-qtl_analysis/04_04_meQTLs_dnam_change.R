bc9b37b65d4bb493be4610f9adf3dd85946fc4ab
Hryhorzhevska <anastasiia_hry@PCR183.local>
2021-12-12 21:27:10 +0100

    me-qtks methylation fold change fixed

--- code/integrative/eqtl/04_me-qtl_analysis/04_04_meQTLs_dnam_change.R
+++ code/integrative/eqtl/04_me-qtl_analysis/04_04_meQTLs_dnam_change.R
@@ -6,39 +6,42 @@ library(dplyr)
 # Main function
 ## ---------
 
-GetMethylChange <- function(snp, meqtl.df, methyl.beta.df, treatment = "dex"){
-  snp.sub.df       <- snp.df[SNP %in% snp]
-  meqtl.snp.cpg.id <- meqtl.df[SNP %in% snp.sub.df$SNP]
+GetMethylChange <- function(meqtl.select, snp.df, methyl.df, treatment = "dex"){
   
-  sample.id.ref   <- colnames(snp.sub.df)[snp.sub.df == 0]
-  homozyg.ref <- methyl.beta.df[rownames(methyl.beta.df) %in% meqtl.snp.cpg.id$CpG_ID, .(ref_mean = rowMeans(.SD)), .SDcols = sample.id.ref]
+  snp.id <- meqtl.select[1] #"rs79375669" # "rs10460588"
+  cpg.id <- meqtl.select[2] #"cg02184558" # 
   
-  sample.id.non.ref       <- colnames(snp.sub.df)[snp.sub.df == 2]
-  homozyg.non.ref <- methyl.beta.df[rownames(methyl.beta.df) %in% meqtl.snp.cpg.id$CpG_ID, .(non_ref_mean = rowMeans(.SD)), .SDcols = sample.id.non.ref]
+  snp.layer <- snp.df[SNP == snp.id]
+  methyl.layer <- methyl.df[CpG_ID == cpg.id]
   
-  sample.id.heter <- colnames(snp.sub.df)[snp.sub.df == 1]
-  heterozyg <- methyl.beta.df[rownames(methyl.beta.df) %in% meqtl.snp.cpg.id$CpG_ID, .(heterozyg_mean = rowMeans(.SD)), .SDcols = sample.id.heter]
+  meqtl <- data.table(t(rbind(snp.layer[, -1], methyl.layer[, -1])))
+  colnames(meqtl) <- c("SNP", "CpG")
   
-  res <- cbind(meqtl.snp.cpg.id, heterozyg, homozyg.ref, homozyg.non.ref)
+  cnt.df <- meqtl[, .(mean = mean(CpG)), by = SNP]
   
-  res[is.na(ref_mean), ref_mean := 0 ]
-  res[is.na(non_ref_mean), non_ref_mean := 0 ]
-  
-  res[, methyl_change := ref_mean - non_ref_mean]
-  res[, ref_mean_darina := (2 * ref_mean + heterozyg_mean) / (2 * (length(sample.id.ref) + length(sample.id.heter)))]
-  res[, non_ref_mean_darina := (2 * non_ref_mean + heterozyg_mean) / (2 * (length(sample.id.non.ref) + length(sample.id.heter)))]
-  res[, methyl_change_darina := ref_mean_darina - non_ref_mean_darina]
-  res[, treatment := treatment]
+  if(nrow(cnt.df) == 3){
+    cnt.df <- cnt.df[SNP == 0, col := "ref_mean"]
+    cnt.df <- cnt.df[SNP == 1, col := "heterozyg_mean"]
+    cnt.df <- cnt.df[SNP == 2, col := "non_ref_mean"]
+    
+    res <- data.table(t(cnt.df$mean))
+    colnames(res) <- cnt.df$col 
+    res[, methyl_change := non_ref_mean - ref_mean]
+    
+    res[, SNP := snp.layer[, 1]]
+    res[, CpG_ID := methyl.layer[, 1]]
+    res[, treatment := treatment]
+  }
 }
 
 ## ---------
 # Set up parameters
 ## ---------
 
-eqtm.in.pre  <- "~/bio/code/mpip/dex-stim-human-array/data/integrative/matrixEQTL/"
+# eqtm.in.pre  <- "~/bio/code/mpip/dex-stim-human-array/data/integrative/matrixEQTL/"
 eqtm.in.pre  <- "/home/ahryhorzhevska/mpip/bio/code/mpip/dex-stim-human-array/data/integrative/matrixEQTL/"
 
-out.dir.pre  <- "~/bio/code/mpip/dex-stim-human-array/output/data/integrative/matrixEQTL/"
+# out.dir.pre  <- "~/bio/code/mpip/dex-stim-human-array/output/data/integrative/matrixEQTL/"
 out.dir.pre  <- "/home/ahryhorzhevska/mpip/bio/code/mpip/dex-stim-human-array/output/data/integrative/matrixEQTL/"
 
 snp.df.fn <- paste0(eqtm.in.pre, "snp_mtrx.csv")
@@ -60,15 +63,10 @@ meqtl.ind.df  <- meqtl.df[SNP %in% snp.lst$SNP]
 
 methyl.beta.df.fn <- paste0(eqtm.in.pre, "methyl_beta_mtrx_", treatment, ".csv")
 methyl.beta.df    <- fread(methyl.beta.df.fn) 
-methyl.beta.df    <- methyl.beta.df[CpG_ID %in% meqtl.ind.df$CpG_ID]
-rownames(methyl.beta.df) <- methyl.beta.df$CpG_ID
 
-methyl.change.dex.df <- do.call("rbind", apply(snp.lst, 1, GetMethylChange, meqtl.ind.df, methyl.beta.df, treatment))
-# methyl.change.dex.df[is.na(methyl.change.dex.df$ref_mean), "ref_mean" ] <- 0
+methyl.change.dex.df <- do.call("rbind", apply(meqtl.ind.df, 1, GetMethylChange, snp.df, methyl.beta.df, treatment))
 
-write.table(methyl.change.dex.df, file = paste0(out.dir.pre, "me-qtl_cis_ind_methyl_change_", treatment, "_fdr_005.csv"), row.names = F, quote = F, sep = "\t", col.names = T, append = F)
-
-x <- do.call("rbind", apply(snp.lst[1], 1, GetMethylChange, meqtl.ind.df, methyl.beta.df, treatment))
+write.table(methyl.change.dex.df, file = paste0(out.dir.pre, "me-qtl_cis_indp_methyl_change_", treatment, "_fdr_005.csv"), row.names = F, quote = F, sep = "\t", col.names = T, append = F)
 
 ## ---------
 # Baseline
@@ -82,15 +80,12 @@ snp.lst      <- fread(snp.lst.fn, col.names = "SNP")
 meqtl.fn  <- paste0(out.dir.pre, "me-qtl_cis_result_", treatment, "_fdr_005.csv")
 col.names <-  c("SNP", "CpG_ID", "beta", "t-stat", "p-value", "fdr")
 meqtl.df  <- fread(meqtl.fn, col.names = col.names)
-
 meqtl.ind.df  <- meqtl.df[SNP %in% snp.lst$SNP]
 
 methyl.beta.df.fn <- paste0(eqtm.in.pre, "methyl_beta_mtrx_", treatment, ".csv")
 methyl.beta.df    <- fread(methyl.beta.df.fn) 
-methyl.beta.df    <- methyl.beta.df[CpG_ID %in% meqtl.ind.df$CpG_ID]
-rownames(methyl.beta.df) <- methyl.beta.df$CpG_ID
 
-methyl.change.veh.df <- do.call("rbind", apply(snp.lst, 1, GetMethylChange, meqtl.ind.df, methyl.beta.df, treatment))
+methyl.change.veh.df <- do.call("rbind", apply(meqtl.ind.df, 1, GetMethylChange, snp.df, methyl.beta.df, treatment))
 
 write.table(methyl.change.veh.df,
-            file = paste0(out.dir.pre, "me-qtl_cis_ind_methyl_change_", treatment, "_fdr_005.csv"), row.names = F, quote = F, sep = "\t", col.names = T, append = F)
+            file = paste0(out.dir.pre, "me-qtl_cis_indp_methyl_change_", treatment, "_fdr_005.csv"), row.names = F, quote = F, sep = "\t", col.names = T, append = F)

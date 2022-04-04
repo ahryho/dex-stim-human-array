a08924e6c00e477b98b52a3e405a0a0ffc7e85de
ahryho <anasastiia.hryhorzhevska@gmail.com>
2022-04-04 16:32:38 +0200

    updates

--- code/integrative/eqtm/03_run_eqtm.sh
+++ code/integrative/eqtm/03_run_eqtm.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-if [ $# -ne 6 ]; then
+if [ $# -ne 7 ]; then
     echo "$0: usage: Not enough arguments
           First argument: treatment (veh, dex, delta)
           Second argument: source folder path 
@@ -27,6 +27,6 @@ out_fn=$job_name.out
 
 sbatch --job-name=$job_name --part=$partition \
   --nodelist=$partition$node --mem=$memory --output=$out_fn \
-  --wrap="Rscript --vanilla $r_script $src_dir $rslt_dir"
+  --wrap="Rscript --vanilla $r_script $treatment $src_dir $rslt_dir"
 
-# ./03_run_eqtl.sh dex /binder/mgp/workspace/2020_DexStim_Array_Human/dex-stim-human-array/data/integrative/matrixEQTL/ /binder/mgp/workspace/2020_DexStim_Array_Human/dex-stim-human-array/output/data/integrative/matrixEQTL/eqtms/ pe 7 400Gb
+# ./03_run_eqtm.sh dex /binder/mgp/workspace/2020_DexStim_Array_Human/dex-stim-human-array/data/integrative/matrixEQTL/ /binder/mgp/workspace/2020_DexStim_Array_Human/dex-stim-human-array/output/data/integrative/matrixEQTL/eqtms/ pe 7 400Gb 03_run_eqtm.R

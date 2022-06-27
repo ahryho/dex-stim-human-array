d4ebdc19919157308a6482a209d065d67d11bc93
Anastasiia Hryhorzhevska <anastasiia_hry@psych.mpg.de>
2022-06-27 07:28:20 +0200

    updates for meQTL CpGs annotation and enrichment

--- code/integrative/meqtl/04_me-qtl_analysis/04_04_me-qtl_cpg_annotation/run_enrichment.R
+++ code/integrative/meqtl/04_me-qtl_analysis/04_04_me-qtl_cpg_annotation/run_enrichment.R
@@ -14,6 +14,8 @@ out.dir.pre  <- "~/bio/code/mpip/dex-stim-human-array/output/data/integrative/ma
 
 meqtls.cpg.delta.coord.gr <- readRDS(paste0(out.dir.pre, "meqtl_delta_cpgs_gr.rds"))
 meqtls.cpg.veh.coord.gr   <- readRDS(paste0(out.dir.pre, "meqtl_veh_cpgs_gr.rds"))
+
+all.cpg.coord.gr          <- readRDS(paste0(out.dir.pre, "all_cpgs_gr.rds"))
 anno.epic.gr              <- readRDS(paste0(out.dir.pre, "anno_epic_gr.rds"))    
 
 features.lst <- elementMetadata(meqtls.cpg.delta.coord.gr)[, "Relation_to_Island"] %>% unique() %>% sort()
@@ -47,6 +49,70 @@ gen.loc.enrich.perm.rslt
 
 fwrite(gen.loc.enrich.perm.rslt, 
        file = paste0("~/bio/code/mpip/dex-stim-human-array/output/data/integrative/matrixEQTL/05_me-qtl_enrichment/region_wise_independent_snps/",  "meqtl_cpgs_relation_to_island_enrichment_perm.csv"), 
+       row.names = F, quote = F)
+
+# Relation to island
+
+no.cores <- detectCores() - 2
+cl <- makeCluster(no.cores)
+registerDoParallel(cl)
+
+nperm   <- 20
+
+gen.loc.enrich.perm.rslt <- foreach(i =  seq_along(features.lst), 
+                                    .combine = rbind, 
+                                    .packages =  c("GenomicRanges", "dplyr")) %dopar% {
+                                      state <- features.lst[i]                                     
+                                      public <- anno.epic.gr[(elementMetadata(anno.epic.gr)[, "Relation_to_Island"]) == state, ] 
+                                      EnrichmentWithPermutationGeneLocWithoutMAF(own = meqtls.cpg.delta.coord.gr,
+                                                                                 background = all.cpg.coord.gr,
+                                                                                 public = public,
+                                                                                 nperm = nperm)
+                                    }
+
+stopImplicitCluster()
+
+gen.loc.enrich.perm.rslt <- cbind(gen.loc.enrich.perm.rslt %>% data.frame(row.names = NULL), 
+                                  Feature = features.lst)
+
+gen.loc.enrich.perm.rslt[["n_perm"]] <- nperm
+
+gen.loc.enrich.perm.rslt
+
+fwrite(gen.loc.enrich.perm.rslt, 
+       file = paste0("~/bio/code/mpip/dex-stim-human-array/output/data/integrative/matrixEQTL/05_me-qtl_enrichment/region_wise_independent_snps/",  "meqtl_cpgs_relation_to_island_enrichment_perm_delta_vs_all.csv"), 
+       row.names = F, quote = F)
+
+# Relation to island
+
+no.cores <- detectCores() - 2
+cl <- makeCluster(no.cores)
+registerDoParallel(cl)
+
+nperm   <- 20
+
+gen.loc.enrich.perm.rslt <- foreach(i =  seq_along(features.lst), 
+                                    .combine = rbind, 
+                                    .packages =  c("GenomicRanges", "dplyr")) %dopar% {
+                                      state <- features.lst[i]                                     
+                                      public <- anno.epic.gr[(elementMetadata(anno.epic.gr)[, "Relation_to_Island"]) == state, ] 
+                                      EnrichmentWithPermutationGeneLocWithoutMAF(own = meqtls.cpg.veh.coord.gr,
+                                                                                 background = all.cpg.coord.gr,
+                                                                                 public = public,
+                                                                                 nperm = nperm)
+                                    }
+
+stopImplicitCluster()
+
+gen.loc.enrich.perm.rslt <- cbind(gen.loc.enrich.perm.rslt %>% data.frame(row.names = NULL), 
+                                  Feature = features.lst)
+
+gen.loc.enrich.perm.rslt[["n_perm"]] <- nperm
+
+gen.loc.enrich.perm.rslt
+
+fwrite(gen.loc.enrich.perm.rslt, 
+       file = paste0("~/bio/code/mpip/dex-stim-human-array/output/data/integrative/matrixEQTL/05_me-qtl_enrichment/region_wise_independent_snps/",  "meqtl_cpgs_relation_to_island_enrichment_perm_veh_vs_all.csv"), 
        row.names = F, quote = F)
 
 # ChIPSeeker 
